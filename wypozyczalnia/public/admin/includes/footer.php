9ae4436536092a9d05866fd61a23d01a8ac8dba3
fckyy <62839760+fckyy@users.noreply.github.com>
2020-06-17 16:52:28 +0200

    Add files via upload

---
+++ wypozyczalnia/public/admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2020 MID-CONTINENT Public Library |<a href="https://piszkod.tk/" target="_blank"  > Customized by : BLipinski</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
